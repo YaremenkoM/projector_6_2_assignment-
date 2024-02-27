62ed727dad17dde5e2bf1feb89919925675ce8c8
YaremenkoM <mariayaremenko13@gmail.com>
2024-02-28 00:01:45 +0100

    refactor to regex

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import csv
 from convertor import distance as d, temperature as t
 import re
+from pathlib import Path
 
 
 def read_file(file: str):
@@ -19,31 +20,42 @@ def format_string(string: str) -> str:
     return f'"{string}"'
 
 
+def extract_digits(string: str) -> float:
+    return float(re.search(r'(\d+)', string).group())
+
+
+def extract_paramter(string: str) -> str:
+    return re.search(r'([a-z]+)', string, re.IGNORECASE).group()
+
+
 def convert_temperature(temperature_str: str, target_parameter: str) -> str:
-    current_parameter = temperature_str[-1:]
-    if current_parameter.lower() == target_parameter.lower():
+    target_parameter = target_parameter.lower()
+    current_parameter = extract_paramter(temperature_str).lower()
+
+    if current_parameter == target_parameter:
         return format_string(temperature_str)
     else:
-        number_to_convert = int(temperature_str[:-2])
-        if target_parameter.lower() == 'c':
+        number_to_convert = extract_digits(temperature_str)
+        if target_parameter == 'c':
             converted_number = t.fahrenheit_to_celsius(number_to_convert)
             return format_string(f'{converted_number}°C')
-        elif target_parameter.lower() == 'f':
+        elif target_parameter == 'f':
             converted_number = t.celsius_to_fahrenheit(number_to_convert)
             return format_string(f'{converted_number}°F')
 
 
 def convert_distance(distance_str: str, target_parameter: str) -> str:
-    current_parameter = distance_str[-1:]
-    if current_parameter.lower() == target_parameter[-1:].lower():
-        return distance_str
+    target_parameter = target_parameter.lower()
+    current_parameter = extract_paramter(distance_str).lower()
+
+    if current_parameter == target_parameter:
+        return format_string(distance_str)
     else:
-        if target_parameter.lower() == 'ft':
-            number_to_convert = int(distance_str[:-1])
+        number_to_convert = extract_digits(distance_str)
+        if target_parameter == 'ft':
             converted_number = d.meters_to_feet(number_to_convert)
             return format_string(f'{converted_number}ft')
-        elif target_parameter.lower() == 'm':
-            number_to_convert = int(distance_str[:-2])
+        elif target_parameter == 'm':
             converted_number = d.feet_to_meters(number_to_convert)
             return format_string(f'{converted_number}m')
 
@@ -67,4 +79,6 @@ def convert_parameters(data, distance_parameter: str, temperature_parameter: str
 
 data = read_file("dataset.csv")
 new_data = convert_parameters(data, distance_parameter='ft', temperature_parameter='C')
+
+Path("./out").mkdir(parents=True, exist_ok=True)
 write_to_file(new_data, 'out/converted_data.csv')
